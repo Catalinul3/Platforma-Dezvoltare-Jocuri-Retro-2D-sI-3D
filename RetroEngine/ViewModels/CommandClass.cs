00b066be8e19bca49e44625a3caf8067e6d6ff66
Catalinul <115569824+Catalinul3@users.noreply.github.com>
2024-06-24 18:09:57 +0300

    connect buttons in retro hub

--- RetroEngine/ViewModels/CommandClass.cs
+++ RetroEngine/ViewModels/CommandClass.cs
@@ -4,11 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace RetroEngine.ViewModels
 {
     public class CommandClass:BaseVM
     {
         private RelayCommand _createGame;
+        private RelayCommand _exitGame;
+        public RelayCommand createGame
+        {
+            get
+            {
+                if(_createGame == null)
+                    _createGame = new RelayCommand(Create);
+                return _createGame;
+            }
+        }
+        public RelayCommand exitGame
+        {
+            get
+            {
+                if(_exitGame==null)
+                    
+                    _exitGame = new RelayCommand(Exit);
+                return _exitGame;
+            }
+        }
+
+        private void Exit(object obj)
+        {
+            MessageBox.Show("You gave up");
+        }
+
+        private void Create(object obj)
+        {
+            MessageBox.Show("Alright");
+        }
     }
 }
--- RetroEngine/ViewModels/MainWindowVM.cs
+++ RetroEngine/ViewModels/MainWindowVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RetroEngine.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,34 @@ using System.Threading.Tasks;
 
 namespace RetroEngine.ViewModels
 {
-    public class MainWindowVM:BaseVM
+    public class MainWindowVM : BaseVM
     {
-        private string _searchBar;
-        public string SearchBar{
-            get { return _searchBar; }
-            set
+        private CommandClass _buttons;
+        private RelayCommand _create;
+        private RelayCommand _exit;
+
+        public RelayCommand create
+        {
+            get
             {
-                _searchBar = value;
-                NotifyPropertyChanged("SearchBar");
+                if (_create == null)
+                    _create = _buttons.createGame;
+                return _create;
+            }
+
+        }
+        public RelayCommand exit
+        {
+            get
+            {
+                if (_exit == null)
+                    _exit = _buttons.exitGame;
+                return _exit;
             }
         }
         public MainWindowVM()
         {
-            _searchBar = "../images/find.png";
+            _buttons=new CommandClass();
         }
     }
 }
